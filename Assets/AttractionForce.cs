63732cf6854e73ae5d0a5f4a7ed5d0e9bdb4d88e
Vivek Raman <vr.ac4bf@live.com>
2019-07-04 19:35:37 +0530

    Initial commit

---
+++ Assets/AttractionForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AttractionForce : MonoBehaviour
+{
+    public float ForceMagnitude = 500f;
+
+    private Vector3 target = Vector3.zero;
+    private Rigidbody rb = null;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(this.transform.localPosition * -ForceMagnitude * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/SpawnerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    [Range(0f, 1f)][SerializeField] private float TimeBetweenSpawns = 0.2f;
+    [SerializeField] private List<GameObject> Balls = null;
+    [SerializeField] private Transform Spawnpoints = null;
+
+    [HideInInspector] public int BallCount = 0;
+    private int count = 0;
+
+    private void Start()
+    {
+        count = Spawnpoints.childCount;
+        if (Input.GetMouseButtonDown(0))
+        InvokeRepeating("Spawn", 0f, TimeBetweenSpawns);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            InvokeRepeating("Spawn", 0f, TimeBetweenSpawns);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CancelInvoke();
+        }
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPosition = Spawnpoints.GetChild(Random.Range(0, count)).position;
+        Instantiate(Balls[BallCount % 2], spawnPosition, Quaternion.identity, this.transform);
+        BallCount++;
+    }
+}
